6ffa20762d28f40dd7a216e4b87f9bc72d31c722
Greg <retgunner@gmail.com>
2022-07-01 19:11:17 +0300

    db connect ok

--- srcs/requirements/wordpress/conf/run.sh
+++ srcs/requirements/wordpress/conf/run.sh
@@ -4,7 +4,6 @@ chown -R www-data:www-data /var/www/*;
 chown -R 755 /var/www/*;
 mkdir -p /run/php/;
 touch /run/php/php7.3-fpm.pid;
-echo "Wordpress: setting up..."
 mkdir -p /var/www/html
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar;
 chmod +x wp-cli.phar; 
@@ -12,9 +11,8 @@ mv wp-cli.phar /usr/local/bin/wp;
 cd /var/www/html;
 wp core download --allow-root;
 mv /var/www/wp-config.php /var/www/html/
-echo "Wordpress: creating users..."
 wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
 wp user create --allow-root ${WP_USER_LOGIN} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD};
-echo "Wordpress: set up!"
+echo "Wordpress OK"
 
 exec "$@"
