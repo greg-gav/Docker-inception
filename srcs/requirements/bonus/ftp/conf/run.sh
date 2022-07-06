1cec5273a532c22fa3124c296e91166f3e398014
Greg <retgunner@gmail.com>
2022-07-06 21:12:31 +0300

    ftp pass and env

--- srcs/requirements/bonus/ftp/conf/run.sh
+++ srcs/requirements/bonus/ftp/conf/run.sh
@@ -12,6 +12,13 @@ vsftpd_stop() {
   echo "Done"
 }
 
+adduser ${FTP_USER} --disabled-password
+echo "${FTP_USER}:${FTP_PASSWORD}" | chpasswd
+echo ${FTP_USER} >> /etc/vsftpd.userlist
+
+mkdir -p /var/www/html && mkdir -p /var/ftp
+chown -R ${FTP_USER}:${FTP_USER} /var/www/html
+
 if [[ "${1}" == "vsftpd" ]]; then
   trap vsftpd_stop SIGINT SIGTERM
   echo "Running ${*}"
