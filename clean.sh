318c443a2daac5fa11bc76aadc1854322355720c
Greg <retgunner@gmail.com>
2022-07-07 17:38:12 +0300

    dirs

--- clean.sh
+++ clean.sh
@@ -1,3 +1,5 @@
 docker stop $(docker ps -qa); docker rm $(docker ps -qa);
 docker rmi -f $(docker images -qa); docker volume rm $(docker volume ls -q);
 docker network rm $(docker network ls -q) 2>/dev/null
+sudo rm -rf /home/bteak/data/*
+sudo mkdir -p /home/bteak/data/wp && mkdir -p /home/bteak/data/db
